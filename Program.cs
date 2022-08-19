956d756f1d9420a016e5a0ad397290ea9d26361f
Emrul <emrul.hoque@gmail.com>
2022-08-19 10:01:51 +0600

    test

--- Program.cs
+++ Program.cs
@@ -4,8 +4,14 @@
     {
         static void Main(string[] args)
         {
+            /*
+            * 1. Add to stage: git add -A
+            * 2. Commit: git commit -m "summary of the changes"
+            * 3. Push: git push
+            */
+
             User userOne = new(){ Id = 132, Name = "Jabir"};
-            Console.WriteLine($" Hello {userOne.Name}!");
+            Console.WriteLine($"Hello {userOne.Name}!");
         }
     }
 }
