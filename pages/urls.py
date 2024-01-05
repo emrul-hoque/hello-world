281e787c77a7f34b440b48e0c06e371b7f0d6ec6
Emrul <emrul.hoque@gmail.com>
2024-01-05 19:17:16 +0600

    initial commit

--- pages/urls.py
+++ pages/urls.py
@@ -1,8 +1,9 @@
 # pages/urls.py
 from django.urls import path
-
-from .views import home_page_view
+from .views import HomePageView, AboutPageView
+#from .views import home_page_view
 
 urlpatterns = [
-    path("", home_page_view, name="home"),
+    path("", HomePageView.as_view(), name="home"),
+    path("about/", AboutPageView.as_view(), name="about"),
 ]
